f2e6e8b0a027d3ba6685c97f3f8e8ff00f371eed
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-08-22 06:19:48 -0600

    ..

--- demo_users/tasks/views.py
+++ demo_users/tasks/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, ListView,UpdateView,DeleteView
 from .models import Task
-from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from users.models import CustomUser
 
-class HomePageView(ListView):
+class HomePageView(LoginRequiredMixin,ListView):
     model = Task
     template_name = 'home.html'
     context_object_name ='tasks'
-
+    login_url = '/accounts/login/'
+    
     def get_queryset(self):
-        if self.request.user.is_authenticated:
-            if self.request.user.is_superuser:
-                return self.model.objects.order_by('-date')
-            else:
-                return self.model.objects.filter(employee=self.request.user)
+        if self.request.user.is_superuser:
+            return self.model.objects.order_by('-date')
         else:
-            self.template_name='error.html'
+            return self.model.objects.filter(employee=self.request.user)
+
 class CreateTask(CreateView):
    
     model= Task
